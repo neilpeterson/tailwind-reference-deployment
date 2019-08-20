8bcba2ec434cfb103504add4829a062eda715bdd
Neil Peterson <neilpeterson@outlook.com>
2019-08-20 12:47:46 -0700

    TSTR

--- deployment-artifacts-standalone-azure-linux-vm/deploy.sh
+++ deployment-artifacts-standalone-azure-linux-vm/deploy.sh
@@ -18,9 +18,9 @@ echo "*************** Container logs ***************"
 echo "az container logs --name bootstrap-container --resource-group $azureResourceGroup --follow"
 echo "*************** Connection Information ***************"
 
-# Create Azure Cosmos DB
-az cosmosdb create --name $randomName --resource-group $azureResourceGroup --kind MongoDB
-cosmosConnectionString=$(az cosmosdb list-connection-strings --name $randomName --resource-group $azureResourceGroup --query connectionStrings[0].connectionString -o tsv)
+# # Create Azure Cosmos DB
+# az cosmosdb create --name $randomName --resource-group $azureResourceGroup --kind MongoDB
+# cosmosConnectionString=$(az cosmosdb list-connection-strings --name $randomName --resource-group $azureResourceGroup --query connectionStrings[0].connectionString -o tsv)
 
 # Create Azure SQL Insance
 az sql server create --location $locaton --resource-group $azureResourceGroup --name $randomName --admin-user $adminUser --admin-password $adminPassword
@@ -32,7 +32,7 @@ sqlConnectionString=$(az sql db show-connection-string --server $randomName --na
 az vm create --name $randomName --resource-group $azureResourceGroup --image UbuntuLTS --admin-username $adminUser --admin-password $adminPassword
 az vm open-port --port 80 --resource-group $azureResourceGroup --name $randomName
 az vm extension set --name customScript --publisher Microsoft.Azure.Extensions --vm-name $randomName --resource-group $azureResourceGroup \
-  --settings '{"fileUris":["https://raw.githubusercontent.com/neilpeterson/tailwind-reference-deployment/master/deployment-artifacts-standalone-azure-linux-vm/config-linux.sh"],"commandToExecute": "./config-linux.sh '$sqlConnectionString' '$cosmosConnectionString'"}'
+  --settings '{'fileUris':['https://raw.githubusercontent.com/neilpeterson/tailwind-reference-deployment/master/deployment-artifacts-standalone-azure-linux-vm/config-linux.sh'],'commandToExecute': './config-linux.sh $sqlConnectionString'}'
 
 echo "************"
 echo $cosmosConnectionString
