f1806a605ee1feff29fec7274c450cbf15a953e4
Neil Peterson <neilpeterson@outlook.com>
2019-08-20 10:32:22 -0700

    TS5

--- deployment-artifacts-standalone-azure-linux-vm/config-linux.sh
+++ deployment-artifacts-standalone-azure-linux-vm/config-linux.sh
@@ -1,34 +1,34 @@
 # https://www.hanselman.com/blog/PublishingAnASPNETCoreWebsiteToACheapLinuxVMHost.aspx
 
 # Clone Anthony fork / update this once merged
-git clone https://github.com/anthonychu/TailwindTraders-Website.git /tailwind
-cd /tailwind
-git checkout monolith
+# git clone https://github.com/anthonychu/TailwindTraders-Website.git /tailwind
+# cd /tailwind
+# git checkout monolith
 
-# Install dotenet 2.2
-wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-dpkg -i packages-microsoft-prod.deb
-add-apt-repository universe
-apt-get install apt-transport-https
-apt-get update
-apt-get install dotnet-sdk-2.2=2.2.102-1 -y
+# # Install dotenet 2.2
+# wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+# dpkg -i packages-microsoft-prod.deb
+# add-apt-repository universe
+# apt-get install apt-transport-https
+# apt-get update
+# apt-get install dotnet-sdk-2.2=2.2.102-1 -y
 
-# Install node / npn
-apt install nodejs -y
-apt install npm -y
+# # Install node / npn
+# apt install nodejs -y
+# apt install npm -y
 
-# Install NGINX and config reverse proxy
-git clone https://github.com/neilpeterson/tailwind-reference-deployment.git
-apt-get install nginx -y
-service nginx start
-rm /etc/nginx/sites-available/default
-curl https://raw.githubusercontent.com/neilpeterson/tailwind-reference-deployment/master/deployment-artifacts-standalone-azure-linux-vm/default > /etc/nginx/sites-available/default
-nginx -t
-nginx -s reload
+# # Install NGINX and config reverse proxy
+# git clone https://github.com/neilpeterson/tailwind-reference-deployment.git
+# apt-get install nginx -y
+# service nginx start
+# rm /etc/nginx/sites-available/default
+# curl https://raw.githubusercontent.com/neilpeterson/tailwind-reference-deployment/master/deployment-artifacts-standalone-azure-linux-vm/default > /etc/nginx/sites-available/default
+# nginx -t
+# nginx -s reload
 
 echo "************"
+echo $0
 echo $1
-echo $2
 echo "************"
 
 # # Build SQL connection string
