bd63a37ec1363e915c4a95de32c9a121f0bcbb4f
Neil Peterson <neilpeterson@outlook.com>
2019-08-20 14:42:02 -0700

    WW

--- deployment-artifacts-standalone-azure-linux-vm/deploy.sh
+++ deployment-artifacts-standalone-azure-linux-vm/deploy.sh
@@ -37,4 +37,4 @@ echo "************"
 az vm create --name $randomName --resource-group $azureResourceGroup --image UbuntuLTS --admin-username $adminUser --admin-password $adminPassword
 az vm open-port --port 80 --resource-group $azureResourceGroup --name $randomName
 az vm extension set --name customScript --publisher Microsoft.Azure.Extensions --vm-name $randomName --resource-group $azureResourceGroup \
-  --settings '{"fileUris":["https://raw.githubusercontent.com/neilpeterson/tailwind-reference-deployment/master/deployment-artifacts-standalone-azure-linux-vm/config-linux.sh"],"commandToExecute": /"./config-linux.sh $sqlConnectionString/"}'
+  --settings '{"fileUris":["https://raw.githubusercontent.com/neilpeterson/tailwind-reference-deployment/master/deployment-artifacts-standalone-azure-linux-vm/config-linux.sh"],"commandToExecute": ./config-linux.sh '$sqlConnectionString'"}'
